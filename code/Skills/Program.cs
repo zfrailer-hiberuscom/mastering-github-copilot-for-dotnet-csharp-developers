1aec7a6cc256963721bfeeaa0f1d743769feb0a6
zfrailer-hiberuscom <zfrailer@hiberus.com>
2026-04-24 12:20:16 +0000

    Agregar proyecto "Skills" y función "CalculateNumbers" para operaciones matemáticas básicas

---
+++ code/Skills/Program.cs
@@ -0,0 +1,13 @@
+﻿void CalculateNumbers(int var1, int var2)
+{ 
+    int sum = var1 + var2;
+    int difference = var1 - var2;
+    int product = var1 * var2;
+    double quotient = (double)var1 / var2;
+
+    Console.WriteLine($"Sum: {sum}");
+    Console.WriteLine($"Difference: {difference}");
+    Console.WriteLine($"Product: {product}");
+    Console.WriteLine($"Quotient: {quotient}");
+}
+
