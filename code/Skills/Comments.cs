5f379d6b14f62e45b629bca0030b77ca831e13dc
zfrailer-hiberuscom <zfrailer@hiberus.com>
2026-04-24 12:44:31 +0000

    Agregar clase "Comments" con método para generar comentarios sobre habilidades

---
+++ code/Skills/Comments.cs
@@ -0,0 +1,14 @@
+namespace Skills;
+
+public class Comments
+{
+    //create a method that generates a comment for a given skill
+    public string GenerateComment(string skill)
+    {
+        return $"This is a comment for the skill: {skill}";
+    }
+
+
+
+
+}
